7236c94d58b0141c373b0a187e0c28663d1b0df5
Gustavo Henrique Montesião de Sousa <gustavo@claroenigma.com.br>
2019-07-24 16:30:26 -0300

    Function `parse_input` to replace `input`
    The `parse_input` function takes the  information about defined
    arguments into account when parsing the command line arguments.

--- args.lua
+++ args.lua
@@ -25,7 +25,131 @@ local function starts_with_hyphen(text)
 	return text:sub(1,1) == "-"
 end
 
+local function slice(t, first, last)
+	if not last or last > #t then last = #t end
+
+	if first > last then return { slice = slice } end
+
+	local old = getmetatable(t)
+
+	if old then
+		first, last = first + old.first - 1, last + old.first - 1
+		t = old.array
+	end
+
+	local meta = {
+		first = first,
+		last = last,
+		array = t,
+		__index = function(_, i) return t[first + i - 1] end,
+		__len = function() return last - first + 1 end
+	}
+
+	return setmetatable({ slice = slice }, meta)
+end
+
 -- Parses the command line arguments
+function parse_input(cmd_args)
+	local arg = slice(arg, 1, #arg)
+	local positionals = slice(cmd_args, 1, #cmd_args)
+
+	local new_flag, flag_value, new_positional, positional_values
+
+	local new_arg = function()
+		local item = arg[1]
+
+		if not item then return end
+
+		if starts_with_hyphen(item) then
+			return new_flag()
+		end
+
+		return new_positional()
+	end
+
+	new_flag = function()
+		local item = arg[1]
+		arg = arg:slice(2)
+
+		local left, right = split_at_equal_sign(item)
+
+		if left == "help" then
+			return "help"
+		end
+
+		local flag = cmd_args[left]
+
+		if not flag then
+			-- TODO
+			return new_arg()
+		end
+
+		if flag.type == boolean or right then
+			flag:set(right) -- TODO
+			return new_arg()
+		end
+
+		return flag_value(flag)
+	end
+
+	flag_value = function(flag)
+		local item = arg[1]
+
+		if item == "=" then
+			arg = arg:slice(2)
+			item = arg[1]
+		end
+
+		if item and not starts_with_hyphen(item) then
+			flag:set(item) -- TODO
+			arg = arg:slice(2)
+		else
+			-- TODO
+		end
+
+		return new_arg()
+	end
+
+	new_positional = function()
+		local positional = positionals[1]
+
+		if not positional then
+			-- TODO
+			arg = arg:slice(2)
+			return new_arg()
+		end
+
+		positional:add(arg[1])
+		arg = arg:slice(2)
+
+		if positional.many then
+			return positional_values()
+		end
+
+		positionals = positionals:slice(2)
+		return new_arg()
+	end
+
+	positional_values = function()
+		local item = arg[1]
+
+		if not item then return end
+
+		if starts_with_hyphen(item) then
+			positionals = positionals:slice(2)
+			return new_flag()
+		end
+
+		local positional = positionals[1]
+		positional:add(item)
+		arg = arg:slice(2)
+
+		return positional_values()
+	end
+
+	return new_arg()
+end
+
 function input()
 	local flag_value, new_arg
 	local help = false
--- spec/args_spec.lua
+++ spec/args_spec.lua
@@ -1,5 +1,136 @@
 local args = require "args"
 
+insulate("The #parse_input function", function()
+	it("should set all the flags", function()
+		args.arg = { "--um", "=", "1", "--dois=doze", "--tres", "-q=4", "-c=cinco" }
+
+		local um = args.flag "um" { type = args.number }
+		local dois = args.flag "d,dois" { type = args.string }
+		local tres = args.flag "tres" { type = args.boolean }
+		local quatro = args.flag "q,quatro" { type = args.number }
+		local cinco = args.flag "c" { type = args.string }
+		local cmd_args = {
+			um = um,
+			d = dois,
+			dois = dois,
+			tres = tres,
+			q = quatro,
+			quatro = quatro,
+			c = cinco
+		}
+
+		local help = args.parse_input(cmd_args)
+
+		assert.is_nil(help)
+		assert.are.same(1, um.value)
+		assert.are.same("doze", dois.value)
+		assert.are.same(true, tres.value)
+		assert.are.same(4, quatro.value)
+		assert.are.same("cinco", cinco.value)
+	end)
+
+	it("should detect a `help` flag", function()
+		args.arg = { "--um", "=", "1", "--dois=doze", "--help", "-q=4", "-c=cinco" }
+
+		local um = args.flag "um" { type = args.number }
+		local dois = args.flag "d,dois" { type = args.string }
+		local quatro = args.flag "q,quatro" { type = args.number }
+		local cinco = args.flag "c" { type = args.string }
+		local cmd_args = {
+			um = um,
+			d = dois,
+			dois = dois,
+			q = quatro,
+			quatro = quatro,
+			c = cinco
+		}
+
+		local help = args.parse_input(cmd_args)
+
+		assert.are.same("help", help)
+	end)
+
+	it("should set the flags and the positional arguments", function()
+		args.arg = { "--um", "=", "1", "--dois=doze", "entrada", "saida", "--tres" }
+
+		local um = args.flag "um" { type = args.number }
+		local dois = args.flag "d,dois" { type = args.string }
+		local tres = args.flag "tres" { type = args.boolean }
+		local input = args.positional "input" { type = args.string }
+		local output = args.positional "output" { type = args.string }
+		local cmd_args = {
+			input, output,
+
+			um = um,
+			d = dois,
+			dois = dois,
+			tres = tres,
+		}
+
+		local help = args.parse_input(cmd_args)
+
+		assert.is_nil(help)
+		assert.are.same(1, um.value)
+		assert.are.same("doze", dois.value)
+		assert.are.same(true, tres.value)
+		assert.are.same("entrada", input.value)
+		assert.are.same("saida", output.value)
+	end)
+
+	it("should set #many values for a positional argument", function()
+		args.arg = { "--um", "=", "1", "--dois=doze", "doc1", "doc2", "doc3", "--tres" }
+
+		local um = args.flag "um" { type = args.number }
+		local dois = args.flag "d,dois" { type = args.string }
+		local tres = args.flag "tres" { type = args.boolean }
+		local files = args.positional "files" { type = args.string, many = true }
+		local cmd_args = {
+			files,
+
+			um = um,
+			d = dois,
+			dois = dois,
+			tres = tres,
+		}
+
+		local help = args.parse_input(cmd_args)
+
+		assert.is_nil(help)
+		assert.are.same(1, um.value)
+		assert.are.same("doze", dois.value)
+		assert.are.same(true, tres.value)
+		assert.are.same({"doc1", "doc2", "doc3"}, files.value)
+	end)
+
+	it("should parses successfully a misbehaved input", function()
+		args.arg = { "--um", "=", "", "--dois=", "--tres", "-q=", "4", "-c=cinco" }
+
+		local um = args.flag "um" { type = args.string }
+		local dois = args.flag "d,dois" { type = args.string }
+		local tres = args.flag "tres" { type = args.boolean }
+		local quatro = args.flag "q,quatro" { type = args.number }
+		local cinco = args.flag "c" { type = args.string }
+		local cmd_args = {
+			um = um,
+			d = dois,
+			dois = dois,
+			tres = tres,
+			q = quatro,
+			quatro = quatro,
+			c = cinco
+		}
+
+		local help = args.parse_input(cmd_args)
+
+		assert.is_nil(help)
+		assert.are.same("", um.value)
+		assert.are.same("", dois.value)
+		assert.are.same(true, tres.value)
+		assert.are.same(4, quatro.value)
+		assert.are.same("cinco", cinco.value)
+	end)
+end)
+
 insulate("The #input function", function()
 	it("should group args in a normal execution", function()
 		args.arg = { "--um", "=", "1", "--dois=doze", "--tres", "-q=4", "cinco" }
@@ -11,6 +142,23 @@ insulate("The #input function", function()
 			{ name = "dois", value = "doze" },
 			{ name = "tres" },
 			{ name = "q", value = "4" },
+			{ positional = "cinco" }
+		}
+
+		assert.are.same(expected, result)
+		assert.is_falsy(help)
+	end)
+
+	it("should detect a positional argument following a boolean flag", function()
+		args.arg = { "--um", "=", "1", "--dois=doze", "--tres", "-q", "cinco" }
+
+		local result, help = args.input()
+
+		local expected = {
+			{ name = "um", value = "1" },
+			{ name = "dois", value = "doze" },
+			{ name = "tres" },
+			{ name = "q" },
 			{ positional = "cinco" }
 		}
 
