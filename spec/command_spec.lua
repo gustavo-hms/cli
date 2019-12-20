38cfdf5f7654fb3e4e9d30d45cd01764044673a1
Gustavo Henrique Montesião de Sousa <gustavo@claroenigma.com.br>
2019-12-20 16:44:36 -0300

    Port `parse_args` tests from `option` module to `command` module

--- spec/command_spec.lua
+++ spec/command_spec.lua
@@ -1,7 +1,146 @@
-cmd = require "command"
-option = require "option"
+insulate("The #parse_args function", function()
+	local option = require "option"
+
+	it("should set all the flags", function()
+		_G.arg = { "--um", "=", "1", "--dois=doze", "--tres", "-q=4", "-c=cinco" }
+
+		local command = require "command"
+
+    	local um = option.flag "um" { type = option.number }
+    	local dois = option.flag "d,dois" { type = option.string }
+    	local tres = option.flag "tres" { type = option.boolean }
+    	local quatro = option.flag "q,quatro" { type = option.number }
+    	local cinco = option.flag "c" { type = option.string }
+		local cmd = command.anonymous { um, dois, tres, quatro, cinco }
+
+		local help, err = cmd.options:parse_args()
+
+		assert.is_nil(help)
+		assert.is_nil(err)
+		assert.are.same(1, um.value)
+		assert.are.same("doze", dois.value)
+		assert.are.same(true, tres.value)
+		assert.are.same(4, quatro.value)
+		assert.are.same("cinco", cinco.value)
+	end)
+
+	it("should detect a `help` flag", function()
+		_G.arg = { "--um", "=", "1", "--dois=doze", "--help", "-q=4", "-c=cinco" }
+		package.loaded.command = nil
+
+		local command = require "command"
+
+		local um = option.flag "um" { type = option.number }
+		local dois = option.flag "d,dois" { type = option.string }
+		local quatro = option.flag "q,quatro" { type = option.number }
+		local cinco = option.flag "c" { type = option.string }
+		local cmd = command.anonymous { um, dois, tres, quatro, cinco }
+
+		local help, err = cmd.options:parse_args()
+
+		assert.are.same("help", help)
+		assert.is_nil(err)
+	end)
+
+	it("should set the flags and the positional arguments", function()
+		_G.arg = { "--um", "=", "1", "--dois=doze", "entrada", "saida", "--tres" }
+		package.loaded.command = nil
+
+		local command = require "command"
+
+		local um = option.flag "um" { type = option.number }
+		local dois = option.flag "d,dois" { type = option.string }
+		local tres = option.flag "tres" { type = option.boolean }
+		local input = option.positional "input" { type = option.string }
+		local output = option.positional "output" { type = option.string }
+		local cmd = command.anonymous { um, dois, tres, input, output }
+
+		local help, err = cmd.options:parse_args()
+
+		assert.is_nil(help)
+		assert.is_nil(err)
+		assert.are.same(1, um.value)
+		assert.are.same("doze", dois.value)
+		assert.are.same(true, tres.value)
+		assert.are.same("entrada", input.value)
+		assert.are.same("saida", output.value)
+	end)
+
+	it("should set #many values for a positional argument", function()
+		_G.arg = { "--um", "=", "1", "--dois=doze", "doc1", "doc2", "doc3", "--tres" }
+		package.loaded.command = nil
+
+		local command = require "command"
+
+		local um = option.flag "um" { type = option.number }
+		local dois = option.flag "d,dois" { type = option.string }
+		local tres = option.flag "tres" { type = option.boolean }
+		local files = option.positional "files" { type = option.string, many = true }
+		local cmd = command.anonymous { um, dois, tres, files }
+
+		local help, err = cmd.options:parse_args()
+
+		assert.is_nil(help)
+		assert.is_nil(err)
+		assert.are.same(1, um.value)
+		assert.are.same("doze", dois.value)
+		assert.are.same(true, tres.value)
+		assert.are.same({"doc1", "doc2", "doc3"}, files.value)
+	end)
+
+	it("should parses successfully a misbehaved input", function()
+		_G.arg = { "--um", "=", "", "--dois=", "--tres", "-q=", "4", "-c=cinco" }
+		package.loaded.command = nil
+
+		local command = require "command"
+
+		local um = option.flag "um" { type = option.string }
+		local dois = option.flag "d,dois" { type = option.string }
+		local tres = option.flag "tres" { type = option.boolean }
+		local quatro = option.flag "q,quatro" { type = option.number }
+		local cinco = option.flag "c" { type = option.string }
+		local cmd = command.anonymous { um, dois, tres, quatro, cinco }
+
+		local help, err = cmd.options:parse_args()
+
+		assert.is_nil(help)
+		assert.is_nil(err)
+		assert.are.same("", um.value)
+		assert.are.same("", dois.value)
+		assert.are.same(true, tres.value)
+		assert.are.same(4, quatro.value)
+		assert.are.same("cinco", cinco.value)
+	end)
+
+	it("should detect a positional argument following a boolean flag", function()
+		_G.arg = { "--um", "=", "1", "--dois=doze", "--tres", "-q", "quinto" }
+		package.loaded.command = nil
+
+		local command = require "command"
+
+		local um = option.flag "um" { type = option.number }
+		local dois = option.flag "d,dois" { type = option.string }
+		local tres = option.flag "tres" { type = option.boolean }
+		local quatro = option.flag "q,quatro" { type = option.boolean }
+		local cinco = option.positional "cinco" { type = option.string }
+		local cmd = command.anonymous { um, dois, tres, quatro, cinco }
+
+		local help, err = cmd.options:parse_args()
+
+		assert.is_nil(help)
+		assert.is_nil(err)
+		assert.are.same(1, um.value)
+		assert.are.same("doze", dois.value)
+		assert.are.same(true, tres.value)
+		assert.are.same(true, quatro.value)
+		assert.are.same("quinto", cinco.value)
+	end)
+end)
 
 describe("The #command function", function()
+    local cmd = require "command"
+    local option = require "option"
+
 	describe("when setting an argument", function()
 		it("should put a flag on the right place in the table", function()
 			local command = cmd.command {
