1f96727fd80ad7bb2f684b0d8d14e7fcb844a5d4
Gustavo Henrique Montesião de Sousa <gustavo@claroenigma.com.br>
2020-03-28 16:48:34 -0300

    Tests for `help` flag

--- spec/cli_spec.lua
+++ spec/cli_spec.lua
@@ -44,7 +44,7 @@ end)
 insulate("A #program", function()
 	it("should complain if a #mandatory option is missing", function()
 		local errors = require "errors"
-		
+
 		errors.exit_with = function(err)
 			local expected = errors.missing_value("mandatory")
 			assert.are.same(expected, err.error_with_code("missing_value"))
@@ -200,5 +200,182 @@ insulate("A #complete program", function()
 		assert.are.equal("in.txt", inspect.input)
 		assert.are.equal("out.txt", inspect.output)
 		assert.is_falsy(inspect.do_not)
+	end)
+end)
+
+insulate("A #program", function()
+	local new_printer = function()
+		local printer = {
+			output = {},
+		}
+
+		printer.print = function(str)
+			printer.output[#printer.output + 1] = str
+		end
+
+		return printer
+	end
+
+	it("should generate a help message from the spec", function()
+		local errors = require "errors"
+		stub(errors, "exit_with")
+
+		local printer = new_printer()
+		_G.print = printer.print
+
+		_G.arg = {
+			[0] = "compute",
+			[1] = "--help"
+		}
+
+		package.loaded.command = nil
+		package.loaded.cli = nil
+		local cli = require "cli"
+
+		_G.add = cli.command {
+			"Add all the given numbers"
+		}
+
+		_G.max = cli.command {
+			"Find the maximum value"
+		}
+
+		_G.all_above = cli.command {
+			"Print all numbers above the given value",
+
+			cli.flag "c,cutoff" {
+				"The value above which all numbers are retained",
+
+				type = cli.number
+			}
+		}
+
+		cli.program {
+			"A program to compute numbers",
+
+			cli.positional "numbers" {
+				"The numbers to operate upon",
+
+				type = cli.number,
+				many = true
+			}
+		}
+
+		assert.stub(errors.exit_with).was_not.called()
+
+		local expected =
+[[A program to compute numbers
+
+Usage:
+
+    compute add numbers...
+        Add all the given numbers
+  
+    compute max numbers...
+        Find the maximum value
+  
+    compute all-above [options] numbers...
+        Print all numbers above the given value
+
+You can run
+
+    compute <command> --help
+
+to get more details about a specific command.
+
+]]
+
+		assert.are.same(expected, table.concat(printer.output))	
+	end)
+
+	it("should generate a help message for the specified command", function()
+		local errors = require "errors"
+		stub(errors, "exit_with")
+
+		local printer = new_printer()
+		_G.print = printer.print
+
+		_G.arg = {
+			[0] = "compute",
+			[1] = "all-above",
+			[2] = "--help"
+		}
+
+		package.loaded.command = nil
+		package.loaded.cli = nil
+		local cli = require "cli"
+
+		cli.program {
+			"A program to compute numbers",
+
+			cli.positional "numbers" {
+				"The numbers to operate upon",
+
+				type = cli.number,
+				many = true
+			}
+		}
+
+		assert.stub(errors.exit_with).was_not.called()
+
+		local expected =
+[[Print all numbers above the given value
+
+Usage:
+    compute all-above [options] numbers...
+
+Options:
+    -c, --cutoff <number>
+	    The value above which all numbers are retained
+
+Arguments:
+    numbers...
+	    The numbers to operate upon
+]]
+
+		assert.are.same(expected, table.concat(printer.output))	
+	end)
+
+	it("should accept a string as argument", function()
+		local errors = require "errors"
+		stub(errors, "exit_with")
+
+		local printer = new_printer()
+		_G.print = printer.print
+
+		_G.arg = {
+			[0] = "compute",
+			[1] = "--help"
+		}
+
+		package.loaded.command = nil
+		package.loaded.cli = nil
+		local cli = require "cli"
+
+		cli.program "A program to compute numbers"
+
+		assert.stub(errors.exit_with).was_not.called()
+
+		local expected =
+[[A program to compute numbers
+
+Usage:
+
+    compute add
+        Add all the given numbers
+  
+    compute max
+        Find the maximum value
+  
+    compute all-above [options]
+        Print all numbers above the given value
+
+You can run
+
+    compute <command> --help
+
+to get more details about a specific command.
+
+]]
 	end)
 end)
